fe51c69dac17c179f245bd428682f54f86e0d35a
Logan Olson <loganolsonn@gmail.com>
2024-04-16 20:51:45 -0500

    Redirect if session exists

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import { getServerSession } from 'next-auth';
 import { authOptions } from './utils/auth';
 import { BackgroundBeams } from '@/components/background-beams';
 import dynamic from 'next/dynamic';
+import { redirect } from 'next/navigation';
 
 const OnBoardCard = dynamic(() => import('@/components/onboard-card'), {
   ssr: false,
@@ -9,6 +10,9 @@ const OnBoardCard = dynamic(() => import('@/components/onboard-card'), {
 
 export default async function Home() {
   const session = await getServerSession(authOptions);
+  if (session) {
+    return redirect('/pickems');
+  }
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-black">
